1c1e5505acd05deafb23d3eabd4920dff6614f06
Clint Valentine <valentine.clint@gmail.com>
2023-11-08 17:08:10 -0800

    feat: add the first version of nf-dotenv (#1)

---
+++ plugins/nf-dotenv/src/test/nextflow/dotenv/DotenvTest.groovy
@@ -0,0 +1,201 @@
+package nextflow.dotenv
+
+import io.github.cdimascio.dotenv.DotenvException
+import nextflow.Channel
+import nextflow.plugin.Plugins
+import nextflow.plugin.TestPluginDescriptorFinder
+import nextflow.plugin.TestPluginManager
+import nextflow.plugin.extension.PluginExtensionProvider
+import org.pf4j.PluginDescriptorFinder
+import spock.lang.Shared
+import test.Dsl2Spec
+
+import java.nio.file.Path
+
+/** Unit tests for the nf-dotenv plugin that use virtual file systems and mocking to run. */
+class DotenvTest extends Dsl2Spec{
+
+    /** Share the plugin mode across all features in this specification. */
+    @Shared String pluginsMode
+
+    /** Setup the test class by loading all plugins. */
+    def setup() {
+        PluginExtensionProvider.reset()
+        pluginsMode = System.getProperty('pf4j.mode')
+        System.setProperty('pf4j.mode', 'dev')
+        Path root = Path.of('.').toAbsolutePath().normalize()
+        def manager = new TestPluginManager(root) {
+            @Override
+            protected PluginDescriptorFinder createPluginDescriptorFinder() {
+                return new TestPluginDescriptorFinder() {
+                    @Override
+                    protected Path getManifestPath(Path pluginPath) {
+                        return pluginPath.resolve('build/resources/main/META-INF/MANIFEST.MF')
+                    }
+                }
+            }
+        }
+        Plugins.init(root, 'dev', manager)
+    }
+
+    /** Cleanup the test class by unloading and resetting all plugins. */
+    def cleanup() {
+        Plugins.stop()
+        PluginExtensionProvider.reset()
+        pluginsMode ? System.setProperty('pf4j.mode',pluginsMode) : System.clearProperty('pf4j.mode')
+    }
+
+    def 'should have the plugin installed but not imported and raise no exception if a dotenv is not found' () {
+        when:
+            String SCRIPT= '''
+                channel.of('hi-mom')
+            '''
+        and:
+            def result = new MockScriptRunner([:]).setScript(SCRIPT).execute()
+        then:
+            result.val == 'hi-mom'
+            result.val == Channel.STOP
+    }
+
+    def 'should import the plugin and raise an exception if a dotenv is not found' () {
+        when:
+            String SCRIPT = '''
+                include { dotenv } from 'plugin/nf-dotenv'
+                channel.of('hi-mom')
+            '''
+        and:
+            new MockScriptRunner([:]).setScript(SCRIPT).execute()
+        then:
+            thrown DotenvException
+    }
+
+    def 'should import the plugin and raise no exceptions the dotenv is found' () {
+        when:
+            String SCRIPT = '''
+                include { dotenv } from 'plugin/nf-dotenv'
+                channel.of('hi-mom')
+            '''
+            String DOTENV = '''
+                FOO=bar
+            '''
+        and:
+            def result = new MockScriptRunner([:]).setScript(SCRIPT).setDotenv(DOTENV).execute()
+        then:
+            result.val == 'hi-mom'
+            result.val == Channel.STOP
+    }
+
+    def 'should import the plugin and return an empty value for a key that does not exist' () {
+        when:
+            String SCRIPT = '''
+                include { dotenv } from 'plugin/nf-dotenv'
+                channel.of(dotenv('BAZ'))
+            '''
+            String DOTENV = '''
+                FOO=bar
+            '''
+        and:
+            def result = new MockScriptRunner([:]).setScript(SCRIPT).setDotenv(DOTENV).execute()
+        then:
+            result.val == ''
+            result.val == Channel.STOP
+    }
+
+
+    def 'should import the plugin and return a the correct value for a key that does exist' () {
+        when:
+            String SCRIPT = '''
+                include { dotenv } from 'plugin/nf-dotenv'
+                channel.of(dotenv('FOO'))
+            '''
+            String DOTENV = '''
+                FOO=bar
+            '''
+        and:
+            def result = new MockScriptRunner([:]).setScript(SCRIPT).setDotenv(DOTENV).execute()
+        then:
+            result.val == 'bar'
+            result.val == Channel.STOP
+    }
+
+
+    def 'should import the plugin and allow for an override of the dotenv filename relative to the main script' () {
+        when:
+            String SCRIPT = '''
+                include { dotenv } from 'plugin/nf-dotenv'
+                channel.of(dotenv('FOO'))
+            '''
+            String DOTENV = '''
+                FOO=bar
+            '''
+        and:
+            def result = new MockScriptRunner(['dotenv.filename': '.envrc'])
+                .setScript(SCRIPT)
+                .setDotenv(DOTENV, '.envrc')
+                .execute()
+        then:
+            result.val == 'bar'
+            result.val == Channel.STOP
+    }
+
+
+    def 'should import the plugin and allow for an override of the dotenv directory as a child directory to the main script' () {
+        when:
+            String SCRIPT = '''
+                include { dotenv } from 'plugin/nf-dotenv'
+                channel.of(dotenv('FOO'))
+            '''
+            String DOTENV = '''
+                FOO=bar
+            '''
+        and:
+            def result = new MockScriptRunner(['dotenv.relative': 'test'])
+                .setScript(SCRIPT)
+                .setDotenv(DOTENV, DotenvExtension.DEFAULT_FILENAME, 'test')
+                .execute()
+        then:
+            result.val == 'bar'
+            result.val == Channel.STOP
+    }
+
+    def 'should import the plugin and allow for an override of the dotenv directory as a parent directory to the main script' () {
+        when:
+            String SCRIPT = '''
+                include { dotenv } from 'plugin/nf-dotenv'
+                channel.of(dotenv('FOO'))
+            '''
+            String DOTENV = '''
+                FOO=bar
+            '''
+        and:
+            def result = new MockScriptRunner(['dotenv.relative': '../other'])
+                .setScript(SCRIPT)
+                .setDotenv(DOTENV, DotenvExtension.DEFAULT_FILENAME, '../other')
+                .execute()
+        then:
+            result.val == 'bar'
+            result.val == Channel.STOP
+    }
+
+    def 'should import the plugin and allow for an override of the dotenv directory as the same as the env file' () {
+        when:
+            String SCRIPT = '''
+                include { dotenv } from 'plugin/nf-dotenv'
+                channel.of(dotenv('FOO'))
+            '''
+            String DOTENV = '''
+                FOO=bar
+            '''
+        and:
+            def result = new MockScriptRunner(['dotenv.relative': '.'])
+                .setScript(SCRIPT)
+                .setDotenv(DOTENV, DotenvExtension.DEFAULT_FILENAME, '.')
+                .execute()
+        then:
+            result.val == 'bar'
+            result.val == Channel.STOP
+    }
+
+    // TODO: Need a test that tests for default value return from config
+    // TODO: Need a test for environment variable override
+}
---
+++ plugins/nf-dotenv/src/test/nextflow/dotenv/MockHelpers.groovy
@@ -0,0 +1,235 @@
+/* Copyright 2022, Seqera Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nextflow.dotenv
+
+import groovyx.gpars.dataflow.DataflowBroadcast
+import nextflow.Session
+import nextflow.executor.Executor
+import nextflow.executor.ExecutorFactory
+import nextflow.processor.TaskHandler
+import nextflow.processor.TaskMonitor
+import nextflow.processor.TaskRun
+import nextflow.processor.TaskStatus
+import nextflow.script.ChannelOut
+import nextflow.script.ScriptRunner
+import nextflow.script.ScriptType
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+/** A class for mock running of a Nextflow main script. */
+class MockScriptRunner extends ScriptRunner {
+
+    // TODO: Use faster in-memory testing when upstream supports it: https://github.com/cdimascio/dotenv-java/issues/59
+    // /** An in-memory filesystem for unit testing. */
+    // static private FileSystem fs = Jimfs.newFileSystem(Configuration.unix())
+    //
+    // static private Path tmpDir = {
+    //     Path tmp = fs.getPath('/tmp')
+    //     tmp.mkdir()
+    //     Path test = Files.createTempDirectory(tmp, 'test')
+    //     test
+    //  }()
+
+    /** Return a random temporary directory for testing. */
+    static private Path tmpDir = {
+        Path tmp = Path.of(System.getProperty('java.io.tmpdir'))
+        tmp.mkdir()
+        Path test = Files.createTempDirectory(tmp, 'test')
+        test
+    }()
+
+    /** Create a temporary file in the in-memory filesystem for unit tests.
+      *
+      * @pararm name The name of the temporary file.
+      * @param content The content of the temporary file, if any.
+      * @param relative The relative modifier to the temporary file e.g. 'child-folder/'
+      */
+    static private Path createInMemTempFile(String name, String content=null, String relative=null) {
+        Path result
+        if (relative) {
+            tmpDir.resolve(relative).mkdir()
+            result = tmpDir.resolve(relative).resolve(name)
+        } else {
+            result = tmpDir.resolve(name)
+        }
+        if (content) {
+            result.text = content
+        }
+        result
+    }
+
+    /** Instantiate a new mock script runner from a config dictionary. */
+    MockScriptRunner(Map config) {
+
+        super(new MockSession(config))
+    }
+
+    /** Set the script `main.nf` with specific contents. */
+    MockScriptRunner setScript(String str) {
+        Path script = createInMemTempFile('main.nf', str)
+        setScript(script)
+        return this
+    }
+
+    /** Set the configuration file `.env` with specific contents. */
+    MockScriptRunner setDotenv(String str, String filename=DotenvExtension.DEFAULT_FILENAME, String relative=null) {
+        createInMemTempFile(filename, str, relative)
+        return this
+
+    }
+
+    /** Normalize the output of the script result so it is easier to compare. */
+    @Override
+    def normalizeOutput(output) {
+        if (output instanceof ChannelOut) {
+            def list = new ArrayList(output.size())
+            for (int i=0; i<output.size(); i++) {
+                list.add(read0(output[i]))
+            }
+            return list.size() == 1 ? list[0] : list
+        }
+
+        if (output instanceof Object[] || output instanceof List) {
+            def result = new ArrayList<>(output.size())
+            for (def item : output) {
+                ((List)result).add(read0(item))
+            }
+            return result
+        }
+
+        else {
+            return read0(output)
+        }
+    }
+
+    /** Read the dataflow broadcast channel. */
+    private static read0(obj) {
+        if (obj instanceof DataflowBroadcast) {
+            return obj.createReadChannel()
+        }
+        return obj
+    }
+
+}
+
+/** A mock Nextflow session. */
+class MockSession extends Session {
+
+    @Override
+    Session start() {
+        this.executorFactory = new MockExecutorFactory()
+        return super.start()
+    }
+
+    MockSession(Map config) {
+        super(config)
+    }
+}
+
+/** A mock Nextflow executor factory. */
+class MockExecutorFactory extends ExecutorFactory {
+
+    /** The class of executor this factory builds. */
+    @Override
+    protected Class<? extends Executor> getExecutorClass(String executorName) {
+        return MockExecutor
+    }
+
+    /** This is a mock factory so of course we support everything! We're a mock! */
+    @Override
+    protected boolean isTypeSupported(ScriptType type, Object executor) {
+        true
+    }
+}
+
+/** A mock Nextflow executor. */
+class MockExecutor extends Executor {
+
+    /** Signal the executor is finished. */
+    @Override
+    void signal() { }
+
+    /** Create a mock task monitor. */
+    protected TaskMonitor createTaskMonitor() {
+        new MockMonitor()
+    }
+
+    /** Create a mock task handler. */
+    @Override
+    TaskHandler createTaskHandler(TaskRun task) {
+        return new  MockTaskHandler(task)
+    }
+}
+
+/** A mock Nextflow monitor. */
+class MockMonitor implements TaskMonitor {
+
+    /** Schedule a task handler. */
+    void schedule(TaskHandler handler) {
+        handler.submit()
+    }
+
+    /** Remove a task handler. */
+    boolean evict(TaskHandler handler) { void }
+
+    /** Start this task monitor. */
+    TaskMonitor start() { void as TaskMonitor }
+
+    /** Signal the monitor is finished. */
+    void signal() { }
+}
+
+/** A mock Nextflow task handler. */
+class MockTaskHandler extends TaskHandler {
+
+    /** Build a mock Nextflow task handler from a task. */
+    protected MockTaskHandler(TaskRun task) {
+        super(task)
+    }
+
+    /** Submit this task. */
+    @Override
+    void submit() {
+        log.info ">> launching mock task: ${task}"
+        if (task.type == ScriptType.SCRIPTLET) {
+            task.workDir = Paths.get('.').complete()
+            task.stdout = task.script
+            task.exitStatus = 0
+        }
+        else {
+            task.code.call()
+        }
+        status = TaskStatus.COMPLETED
+        task.processor.finalizeTask(task)
+    }
+
+    /** Check if this task is still running but since we are mock, it's totally finished. */
+    @Override
+    boolean checkIfRunning() {
+        false
+    }
+
+    /** Check if this task is completed but since we are mock, it's totally completed. */
+    @Override
+    boolean checkIfCompleted() {
+        true
+    }
+
+    /** Kill the thing that never existed, yeah that's right, kill the void. */
+    @Override
+    void kill() { void }
+}
